4a454ec88c7193ed9d2f1826c971f2ed9671e57b
Matthias Küch <mail@matthias-kuech.de>
2022-08-10 11:10:04 +0200

    Add test

---
+++ test_gt7communication.py
@@ -0,0 +1,14 @@
+import time
+import unittest
+
+import gt7communication
+
+
+class GT7CommunicationTest(unittest.TestCase):
+    def test_run(self):
+        gt7comm = gt7communication.GT7Communication("192.168.178.120")
+        gt7comm.daemon = True
+        gt7comm.start()
+        car_data = gt7comm.get_last_data()
+        # is always 85
+        self.assertEqual(85, car_data.water_temp)
