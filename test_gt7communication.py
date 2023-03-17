9ae4fea9d95935ba8780a97ff3527612d1709c65
Matthias Küch <mail@matthias-kuech.de>
2023-03-17 10:28:07 +0100

    Conditional skip

--- test_gt7communication.py
+++ test_gt7communication.py
@@ -3,11 +3,27 @@ import unittest
 
 import gt7communication
 from gt7lap import Lap
+import socket
 
 PLAYSTATION_IP = "192.168.178.119"
+# # TODO check if this is true
+PLAYSTATION_PORT = 33739
 
 
-# @unittest.skip("Works only with real game running")
+# check if host is up
+def is_host_up(ip: str, port: int) -> bool:
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.settimeout(1)
+        s.connect((ip, port))
+        s.close()
+        return True
+    except socket.error:
+        return False
+
+
+@unittest.skipIf(not is_host_up(PLAYSTATION_IP, PLAYSTATION_PORT),
+                 "Playstation host is not up on %s:%d" % (PLAYSTATION_IP, PLAYSTATION_PORT))
 class GT7CommunicationTest(unittest.TestCase):
     def setUp(self) -> None:
         self.gt7comm = gt7communication.GT7Communication(PLAYSTATION_IP)
@@ -21,6 +37,7 @@ class GT7CommunicationTest(unittest.TestCase):
 
     def tearDown(self) -> None:
         self.gt7comm.stop()
+
     def test_run(self):
         car_data = self.gt7comm.get_last_data()
         self.assertTrue(self.gt7comm.is_connected())
@@ -31,13 +48,8 @@ class GT7CommunicationTest(unittest.TestCase):
         while self.gt7comm.is_connected():
             car_data = self.gt7comm.get_last_data()
             print(car_data.rpm, car_data.in_race)
-        # self.assertTrue(self.gt7comm.is_connected())
-        # is always 85
-        # self.assertEqual(85, car_data.water_temp)
 
     def test_load_laps(self):
-        self.gt7comm = gt7communication.GT7Communication(PLAYSTATION_IP)
-
         self.gt7comm.laps = [Lap()]
         self.gt7comm.laps[0].number = 0
 
