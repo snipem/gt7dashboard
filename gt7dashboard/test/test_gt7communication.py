6328d82d9cb8a6500c46630e416a319fd248a145
Matthias Küch <mail@matthias-kuech.de>
2023-04-19 19:44:47 +0200

    Fix test connection

--- gt7dashboard/test/test_gt7communication.py
+++ gt7dashboard/test/test_gt7communication.py
@@ -1,32 +1,29 @@
+import os
 import time
 import unittest
 
-import gt7dashboard.gt7communication
 from gt7dashboard import gt7communication
 from gt7dashboard.gt7lap import Lap
-import socket
 
 PLAYSTATION_IP = "ps5wifi"
-# # TODO check if this is true
-PLAYSTATION_PORT = 33739
 
 
 # check if host is up
-def is_host_up(ip: str, port: int) -> bool:
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.settimeout(1)
-        s.connect((ip, port))
-        s.close()
+def is_host_up(ip: str) -> bool:
+    response = os.system("ping -c 1 " + PLAYSTATION_IP)
+
+    #and then check the response...
+    if response == 0:
         return True
-    except socket.error:
+    else:
         return False
 
 
-@unittest.skipIf(not is_host_up(PLAYSTATION_IP, PLAYSTATION_PORT),
-                 "Playstation host is not up on %s:%d" % (PLAYSTATION_IP, PLAYSTATION_PORT))
+@unittest.skipIf(not is_host_up(PLAYSTATION_IP),
+                 "Playstation host is not up on %s" % (PLAYSTATION_IP))
 class GT7CommunicationTest(unittest.TestCase):
-    def setUp(self) -> None:
+    @classmethod
+    def setUpClass(self) -> None:
         self.gt7comm = gt7communication.GT7Communication(PLAYSTATION_IP)
         # Do not quit with the main process
         self.gt7comm.daemon = False
@@ -36,19 +33,20 @@ class GT7CommunicationTest(unittest.TestCase):
         while not self.gt7comm.is_connected():
             time.sleep(0.1)
 
-    def tearDown(self) -> None:
+    @classmethod
+    def tearDownClass(self) -> None:
         self.gt7comm.stop()
 
-    def test_run(self):
+    def test_get_water_temp(self):
         car_data = self.gt7comm.get_last_data()
         self.assertTrue(self.gt7comm.is_connected())
         # is always 85
         self.assertEqual(85, car_data.water_temp)
 
-    def test_run_add_debug(self):
-        while self.gt7comm.is_connected():
-            car_data = self.gt7comm.get_last_data()
-            print(car_data.rpm, car_data.in_race)
+    # def test_run_add_debug(self):
+    #     while self.gt7comm.is_connected():
+    #         car_data = self.gt7comm.get_last_data()
+    #         # print(car_data.rpm, car_data.in_race)
 
     def test_load_laps(self):
         self.gt7comm.laps = [Lap()]
