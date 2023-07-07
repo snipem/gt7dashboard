a22f7fa68606bdafec666facf1170dfaa9a6f08d
Matthias Küch <mail@matthias-kuech.de>
2023-07-07 10:52:00 +0200

    Use broadcast

--- gt7dashboard/test/test_gt7communication.py
+++ gt7dashboard/test/test_gt7communication.py
@@ -5,7 +5,7 @@ import unittest
 from gt7dashboard import gt7communication
 from gt7dashboard.gt7lap import Lap
 
-PLAYSTATION_IP = "ps5wifi"
+PLAYSTATION_IP = "255.255.255.255"
 
 
 # check if host is up
